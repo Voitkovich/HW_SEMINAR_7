527366e0f330efe1de99b93d01b0def9e7c943bb
Ekaterina Voitkovich <styardess@mail.ru>
2022-11-20 15:09:13 +0300

    задача 50

--- Задача 50/Program.cs
+++ Задача 50/Program.cs
@@ -13,3 +13,39 @@
 // 8 4 2 4
 
 // M = 1, N = 7 -> В массиве отсутствует элемент в такой позиции.
+void fillArray(int[,] array )
+
+{
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i, j] = new Random().Next(1, 100);
+        Console.Write("{0, 4}", array[i, j] + "\t");
+    }
+    Console.WriteLine();
+}
+}
+int m = 10;
+int n = 10;
+int[,] array = new int[m, n];
+
+fillArray(array);
+Console.WriteLine(array);
+
+Console.Write("Введите М: ");
+int M = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+        if(M < array.GetLength(0) && N < array.GetLength(1) )
+        {
+            Console.Write( $"{array[M, N]}");
+        }
+       else
+        {
+            Console.WriteLine("Элемент отсутствует");
+        }
+        Console.WriteLine();
+
--- Задача 52/Program.cs
+++ Задача 52/Program.cs
@@ -7,3 +7,23 @@
 // 5 9 2 3
 // 8 4 2 4
 // Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+void fillArray(int[,] array )
+
+{
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i, j] = new Random().Next(1, 100);
+        Console.Write("{0, 4}", array[i, j] + "\t");
+    }
+    Console.WriteLine();
+}
+}
+int m = 10;
+int n = 10;
+int[,] array = new int[m, n];
+
+fillArray(array);
+Console.WriteLine(array);
