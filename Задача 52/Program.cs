152acc33da2a790413ebbebdfbfce29c3f7f5ea2
Ekaterina Voitkovich <styardess@mail.ru>
2022-11-20 21:59:42 +0300

    Задача 52

--- Задача 52/Program.cs
+++ Задача 52/Program.cs
@@ -7,9 +7,23 @@
 // 5 9 2 3
 // 8 4 2 4
 // Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+void arithmeticMean(int[,]array)
+
+{
+    double sum = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            sum = sum + array[j, i];
+        }
+        Console.WriteLine();
+        Console.WriteLine(sum / array.GetLength(1));
+        sum = 0;
+    }
+}
 
 void fillArray(int[,] array )
-
 {
 for (int i = 0; i < array.GetLength(0); i++)
 {
@@ -21,9 +35,15 @@ for (int i = 0; i < array.GetLength(0); i++)
     Console.WriteLine();
 }
 }
-int m = 10;
-int n = 10;
+Console.Write("Введите М: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
 int[,] array = new int[m, n];
 
 fillArray(array);
+Console.WriteLine(array);
+arithmeticMean(array);
 Console.WriteLine(array);
